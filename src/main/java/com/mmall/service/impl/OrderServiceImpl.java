0cb136c51100abee867e8989d262a52d64e4090b
877406889 <877406889@qq.com>
2019-05-19 19:06:11 +0800

    开始整合支付宝接口。。有点难

---
+++ src/main/java/com/mmall/service/impl/OrderServiceImpl.java
@@ -0,0 +1,129 @@
+package com.mmall.service.impl;
+
+import com.alipay.demo.trade.config.Configs;
+import com.alipay.demo.trade.model.ExtendParams;
+import com.alipay.demo.trade.model.GoodsDetail;
+import com.alipay.demo.trade.model.builder.AlipayTradePayRequestBuilder;
+import com.alipay.demo.trade.model.result.AlipayF2FPayResult;
+import com.alipay.demo.trade.model.result.AlipayF2FPrecreateResult;
+import com.alipay.demo.trade.service.AlipayTradeService;
+import com.alipay.demo.trade.service.impl.AlipayTradeServiceImpl;
+import com.google.common.collect.Maps;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.OrderItemMapper;
+import com.mmall.dao.OrderMapper;
+import com.mmall.pojo.Order;
+import com.mmall.pojo.OrderItem;
+import com.mmall.service.IOrderService;
+import com.mmall.util.BigDecimalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sun.security.krb5.Config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static com.alipay.demo.trade.Main.tradeService;
+import static com.alipay.demo.trade.model.TradeStatus.FAILED;
+import static com.alipay.demo.trade.model.TradeStatus.SUCCESS;
+
+@Service("iOrderService")
+public class OrderServiceImpl implements IOrderService {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private OrderItemMapper orderItemMapper;
+    
+    public ServerResponse pay(Integer userId,String path,Long orderNo){
+        Map<String,String >resultMap= Maps.newHashMap();
+        Order order=orderMapper.selectByUserIdAndOrderNo(userId,orderNo);
+        if(order==null){
+            return ServerResponse.createByErrorMessage("用户没有该订单");
+        }
+        resultMap.put("order",String.valueOf(order.getOrderNo()));
+        // (必填) 商户网站订单系统中唯一订单号，64个字符以内，只能包含字母、数字、下划线，
+        // 需保证商户系统端不能重复，建议通过数据库sequence生成，
+        String outTradeNo =order.getOrderNo().toString();
+
+        // (必填) 订单标题，粗略描述用户的支付目的。如“xxx品牌xxx门店消费”
+        String subject = new StringBuilder().append("happymall扫码支付，订单号：").append(outTradeNo).toString();
+
+        // (必填) 订单总金额，单位为元，不能超过1亿元
+        // 如果同时传入了【打折金额】,【不可打折金额】,【订单总金额】三者,则必须满足如下条件:【订单总金额】=【打折金额】+【不可打折金额】
+        String totalAmount = order.getPayment().toString();
+
+        // (必填) 付款条码，用户支付宝钱包手机app点击“付款”产生的付款条码
+        String authCode = "用户自己的支付宝付款码"; // 条码示例，286648048691290423
+        // (可选，根据需要决定是否使用) 订单可打折金额，可以配合商家平台配置折扣活动，如果订单部分商品参与打折，可以将部分商品总价填写至此字段，默认全部商品可打折
+        // 如果该值未传入,但传入了【订单总金额】,【不可打折金额】 则该值默认为【订单总金额】- 【不可打折金额】
+        //        String discountableAmount = "1.00"; //
+
+        // (可选) 订单不可打折金额，可以配合商家平台配置折扣活动，如果酒水不参与打折，则将对应金额填写至此字段
+        // 如果该值未传入,但传入了【订单总金额】,【打折金额】,则该值默认为【订单总金额】-【打折金额】
+        String undiscountableAmount = "0";
+
+        // 卖家支付宝账号ID，用于支持一个签约账号下支持打款到不同的收款账号，(打款到sellerId对应的支付宝账号)
+        // 如果该字段为空，则默认为与支付宝签约的商户的PID，也就是appid对应的PID
+        String sellerId = "";
+
+        // 订单描述，可以对交易或商品进行一个详细地描述，比如填写"购买商品3件共20.00元"
+        String body = new StringBuilder().append("订单").append(outTradeNo).append("购买商品共").append(totalAmount).append("元").toString();
+
+        // 商户操作员编号，添加此参数可以为商户操作员做销售统计
+        String operatorId = "test_operator_id";
+
+        // (必填) 商户门店编号，通过门店号和商家后台可以配置精准到门店的折扣信息，详询支付宝技术支持
+        String storeId = "test_store_id";
+
+        // 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)，详情请咨询支付宝技术支持
+        String providerId = "2088100200300400500";
+        ExtendParams extendParams = new ExtendParams();
+        extendParams.setSysServiceProviderId(providerId);
+
+        // 支付超时，线下扫码交易定义为5分钟
+        String timeoutExpress = "5m";
+
+        // 商品明细列表，需填写购买商品详细信息，
+        List<GoodsDetail> goodsDetailList = new ArrayList<GoodsDetail>();
+        List<OrderItem>orderItemList=orderItemMapper.getByOrderNoUserId(orderNo,userId);
+        for (OrderItem orderItem:orderItemList){
+             GoodsDetail goods=GoodsDetail.newInstance(orderItem.getProductId().toString(),orderItem.getProductName(), BigDecimalUtil.mul(orderItem.getCurrentUnitPrice().doubleValue(),new Double(100).doubleValue()).longValue(),orderItem.getQuantity());
+             goodsDetailList.add(goods);
+        }
+
+
+        String appAuthToken = "http://www.happymmall.com/order/alipay_callback.do";//根据真实值填写
+
+        // 创建条码支付请求builder，设置请求参数
+        AlipayTradePayRequestBuilder builder = new AlipayTradePayRequestBuilder()
+                            .setAppAuthToken(appAuthToken)
+                .setOutTradeNo(outTradeNo).setSubject(subject).setAuthCode(authCode)
+                .setTotalAmount(totalAmount).setStoreId(storeId)
+                .setUndiscountableAmount(undiscountableAmount).setBody(body).setOperatorId(operatorId)
+                .setExtendParams(extendParams).setSellerId(sellerId)
+                .setGoodsDetailList(goodsDetailList).setTimeoutExpress(timeoutExpress);
+        Configs.init("zfbinfo.properties");
+        // 调用tradePay方法获取当面付应答
+        AlipayTradeService tradeService=new AlipayTradeServiceImpl.ClientBuilder().build();
+
+        AlipayF2FPrecreateResult result = tradeService.tradePrecreate(builder);
+        switch (result.getTradeStatus()) {
+            case SUCCESS:
+                log.info("支付宝支付成功: )");
+                break;
+
+            case FAILED:
+                log.error("支付宝支付失败!!!");
+                break;
+
+            case UNKNOWN:
+                log.error("系统异常，订单状态未知!!!");
+                break;
+
+            default:
+                log.error("不支持的交易状态，交易返回异常!!!");
+                break;
+        }
+    }
+}
--- src/main/java/com/mmall/service/impl/ShippingService.java
+++ src/main/java/com/mmall/service/impl/ShippingService.java
@@ -1,4 +1,81 @@
 package com.mmall.service.impl;
 
-public class ShippingService {
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.google.common.collect.Maps;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.ShippingMapper;
+import com.mmall.pojo.Shipping;
+import com.mmall.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("iShippingService")
+public class ShippingService implements IShippingService {
+    @Autowired
+    private ShippingMapper shippingMapper;
+
+    /**
+     * 添加地址
+     * @param userId
+     * @param shipping
+     * @return
+     */
+    public ServerResponse add(Integer userId, Shipping shipping){
+        shipping.setUserId(userId);
+        int rowCount=shippingMapper.insert(shipping);
+        if (rowCount>0){
+            Map result= Maps.newHashMap();
+            result.put("shippingId",shipping.getId());
+            //插入成功则返回一个map里面内容为创建成功的地址的id
+            return ServerResponse.createBySuccess("创建地址成功",result);
+        }
+        return ServerResponse.createByErrorMessage("创建地址失败");
+    }
+
+    /**
+     * 删除地址的方法
+     * @param userId 用户Id
+     * @param shippingId 地址Id
+     * @return
+     */
+    public ServerResponse<String> del(Integer userId,Integer shippingId){
+        int resultCount=shippingMapper.deleteByShippingIdUserId(userId,shippingId);
+        if (resultCount>0){
+            return ServerResponse.createBySuccess("删除地址成功");
+        }
+        return ServerResponse.createByErrorMessage("删除地址失败");
+    }
+
+    public ServerResponse update(Integer userId,Shipping shipping){
+        //从shipping里获取而不是用传过来的
+        //防止别人模拟id然后攻击横向越权
+        shipping.setUserId(userId);
+        int rowCount=shippingMapper.updateByShipping(shipping);
+        if (rowCount>0){
+            Map result= Maps.newHashMap();
+            result.put("shippingId",shipping.getId());
+            //插入成功则返回一个map里面内容为创建成功的地址的id
+            return ServerResponse.createBySuccess("更新地址成功");
+        }
+        return ServerResponse.createByErrorMessage("更新地址失败");
+    }
+
+    public ServerResponse<Shipping> select(Integer userId,Integer shippingId){
+        Shipping shipping=shippingMapper.selectByShippingIdUserId(userId,shippingId);
+        if(shipping==null){
+            return ServerResponse.createByErrorMessage("无法查询到该地址");
+        }
+        return ServerResponse.createBySuccess("查询地址成功",shipping);
+    }
+
+    public ServerResponse<PageInfo> list(Integer userId,Integer pageNum,Integer pageSize){
+        PageHelper.startPage(pageNum,pageSize);
+        List<Shipping> shippingList= shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo=new PageInfo(shippingList);
+        return ServerResponse.createBySuccess(pageInfo);
+    }
 }
